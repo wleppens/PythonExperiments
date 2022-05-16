82a42766d9ba9a5576d4bf59071f9f67ae0c291f
wleppens <37867468+wleppens@users.noreply.github.com>
2022-05-16 20:23:03 +0200

    Create switch_config_script.py

---
+++ netmiko/switch_config_script.py
@@ -0,0 +1,16 @@
+from netmiko import ConnectHandler
+
+cisco_Switch = {
+    "device_type": "cisco_ios",
+    "host": "192.168.10.162",
+    "username": "admin",
+    "password": "cisco",
+    "secret": "cisco"}
+
+with ConnectHandler(**cisco_Switch) as net_connect:
+    
+    net_connect.enable()
+    output = net_connect.send_config_from_file('iosv_l3_config.txt')
+
+    print (output)
+    net_connect.disconnect()
